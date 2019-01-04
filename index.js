e1bc89e4743c7c77075b982bda54ac36e4585760
Koen Van Looveren <koen.vanlooveren@icapps.com>
2019-01-04 14:54:30 +0100

    Removed the RNPushnotifications implementation for android. We will try to rebuild with a npm module

--- index.js
+++
@@ -1,5 +0,0 @@
-import { DeviceEventEmitter, NativeModules, Platform } from 'react-native';
-
-const RNPushNotification = Platform.OS === 'ios' ? {} : NativeModules.RnPushNotificationsModule;
-
-export default RNPushNotification;
