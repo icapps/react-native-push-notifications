bba992d1a9922aa578aa95a6d816776d6c09d0d8
Koen Van Looveren <koen.vanlooveren@icapps.com>
2019-01-04 16:27:18 +0100

    Added the firebase token with a react method

--- android/src/main/java/com/icapps/rnpushnotifications/RNPushNotificationsModule.kt
+++ android/src/main/java/com/icapps/rnpushnotifications/RNPushNotificationsModule.kt
@@ -1,21 +1,21 @@
 package com.icapps.rnpushnotifications
 
-import android.util.Log
-
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.icapps.rnpushnotifications.persitance.SharedPrefs
 
 /**
  * @author Koen Van Looveren
  */
 class RNPushNotificationsModule internal constructor(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
+    private val sharedPrefs = SharedPrefs(reactContext)
+
     @ReactMethod
     fun getFirebaseToken(promise: Promise) {
-        promise.resolve("real-token")
-        Log.d(NAME, "token provided")
+        promise.resolve(sharedPrefs.getFirebaseToken())
     }
 
     override fun getName(): String {
